ea0e650f371e001ffc958cbdc731e7fd376fa867
informaticaesne <informatica@esne.es>
2023-11-22 16:20:01 +0100

    Started new PHP file.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+        <link rel="shortcut icon" href="media/favicon.png" type="image/x-icon">
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer">
@@ -12,6 +12,16 @@
     </head>
 
     <body>
+
+        <!-- Para escribir PHP, deberias de cambiar el index a .php y de ahi
+        se procesa todos los comandos sacado de la base de datos. -->
+
+        <!-- Instalando XAMPP y dentro APACHE al abrirlo con localhost, crea este
+        mini servidor -->
+        <?php
+            echo "<p>hello world!11</p>";
+        ?>
+
         <nav id="menu">
             <figure>
                 <a href="#"><img src="media/riot-games-LOGO.svg" alt="riot logo"></a>
